e1ace79fbc106f95347f24ef13e4ec53fa6ec749
IamMaxim <g.maxim.stepanoff@gmail.com>
2019-11-28 23:31:35 +0300

    Updated scripts

--- runPopulation.sh
+++ runPopulation.sh
@@ -4,6 +4,6 @@ clear
 git pull
 python3 populate.py
 echo '   ---   '
-PGPASSWORD=passpass psql -U dmd1_user -d dmd1 -p 18636 -h localhost -a -f SQL/table_creation.sql | grep 'error'
-PGPASSWORD=passpass psql -U dmd1_user -d dmd1 -p 18636 -h localhost -a -f insert_script.sql | grep 'error'
-PGPASSWORD=passpass psql -U dmd1_user -d dmd1 -p 18636 -h localhost -a -f manual_inserts.sql | grep 'error'
+PGPASSWORD=passpass psql -U dmd1_user -d hospital -p 18636 -h localhost -a -f SQL/table_creation.sql | grep 'error'
+PGPASSWORD=passpass psql -U dmd1_user -d hospital -p 18636 -h localhost -a -f insert_script.sql | grep 'error'
+PGPASSWORD=passpass psql -U dmd1_user -d hospital -p 18636 -h localhost -a -f manual_inserts.sql | grep 'error'
