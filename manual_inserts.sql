cf2cdeb35759966c355c41502d51e0b8d34619bc
pollytur <havegreatsuccessq@gmail.com>
2019-11-28 14:11:37 +0300

    manual inserts for select checks

---
+++ manual_inserts.sql
@@ -0,0 +1,138 @@
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (188, 2, 12, '2019-11-1 09:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (33, 2, 1, '2019-10-30 10:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-11-15 13:38:44.882913');
+
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-29 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-5 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-6 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-12 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-13 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-19 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-20 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-26 13:38:44.882913');
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time)
+VALUES (100, 2, 9, '2019-10-27 13:38:44.882913');
+
+-- 1
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2019-10-27 13:38:44.882913', true);
+-- 2
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2018-10-27 13:38:44.882913', true);
+-- 3
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2017-10-27 13:38:44.882913', true);
+
+-- 4
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2016-10-27 13:38:44.882913', true);
+-- 5
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2015-10-27 13:38:44.882913', true);
+--6
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2014-10-27 13:38:44.882913', true);
+--7
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2013-10-27 13:38:44.882913', true);
+-- 8
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2012-10-27 13:38:44.882913', true);
+-- 9
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2011-10-27 13:38:44.882913', true);
+-- 10
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+INSERT INTO appointment(patient_id, rec_id, doctor_id, date_and_time, is_home_visit) VALUES (30, 2, 25, '2010-10-27 13:38:44.882913', true);
+
