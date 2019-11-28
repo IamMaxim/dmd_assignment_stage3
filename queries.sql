6efa0eff7b3dac61ccc5babc52cef58e89fe9424
IamMaxim <g.maxim.stepanoff@gmail.com>
2019-11-28 12:22:39 +0300

    Working second query

--- queries.sql
+++ queries.sql
@@ -17,6 +17,14 @@ order by date_and_time desc
 limit 1;
 $$ language sql;
 
+create or replace function get_data_for_first_query(out p_id int, out d_name varchar(255)) as
+$$
+select (p.id, d.name)
+from ((appointment join patient p on appointment.patient_id = p.id)
+         join doctor d on appointment.doctor_id = d.id);
+$$
+    language sql;
+
 create or replace function first_query(patient_id int, first_letter char(1), second_letter char(1)) returns setof varchar as
 $$
 select name
@@ -34,12 +42,14 @@ $$
 
 
 -- 2
-create or replace function second_query(out timeslot timestamp, out count bigint) as
+create type second_query_result as (timeslot timestamp, count bigint);
+
+create or replace function second_query() returns setof second_query_result as
 $$
 select appointment.date_and_time, count(appointment)
 from appointment
-where date_and_time >= date_trunc('month', current_date - interval '1' month)
-  and date_and_time < date_trunc('month', current_date)
+where date_and_time >= date_trunc('year', current_date - interval '1' year)
+  and date_and_time < date_trunc('year', current_date)
 group by appointment.date_and_time
 $$ language sql;
 
@@ -59,33 +69,34 @@ $$
 
 create or replace function get_prev_month() returns timestamp as
 $$
-    select cast(date_trunc('month', current_date) as date) - interval '1' month
+select cast(date_trunc('month', current_date) as date) - interval '1' month
 $$
     language sql;
 
 create or replace function third_query() returns patient as
 $$
-select * from (
-                  select *
-                  from patient
-                  where number_of_appointments_between(
-                                                        id,
-                                                        (select get_prev_month()),
-                                                        (select get_prev_month() + interval '7' day)
-                                                        ) >= 2
-                    and number_of_appointments_between(id,
-                                                       (get_prev_month() + interval '7' day),
-                                                       (select get_prev_month() + interval '14' day)
-                                                        ) >= 2
-                    and number_of_appointments_between(id,
-                                                       (get_prev_month() + interval '14' day),
-                                                       (select get_prev_month() + interval '21' day)
-                                                        ) >= 2
-                    and number_of_appointments_between(id,
-                                                       (get_prev_month() + interval '21' day),
-                                                       (select get_prev_month() + interval '28' day)
-                                                        ) >= 2
-              ) as result3;
+select *
+from (
+         select *
+         from patient
+         where number_of_appointments_between(
+                       id,
+                       (select get_prev_month()),
+                       (select get_prev_month() + interval '7' day)
+                   ) >= 2
+           and number_of_appointments_between(id,
+                                              (get_prev_month() + interval '7' day),
+                                              (select get_prev_month() + interval '14' day)
+                   ) >= 2
+           and number_of_appointments_between(id,
+                                              (get_prev_month() + interval '14' day),
+                                              (select get_prev_month() + interval '21' day)
+                   ) >= 2
+           and number_of_appointments_between(id,
+                                              (get_prev_month() + interval '21' day),
+                                              (select get_prev_month() + interval '28' day)
+                   ) >= 2
+     ) as result3;
 $$
     language sql;
 
@@ -137,7 +148,34 @@ $$
 
 
 -- 5
-create or replace function fifth_query(period int, patients_per_year int, total_patients int) returns setof doctor as
+-- create or replace function fifth_query(period int, patients_per_year int, total_patients int) returns setof doctor as
+-- $$
+-- SELECT doctor.* /*, patients*/
+-- FROM (
+--          SELECT noapy.doctor_id, SUM(noapy.ct) as patients
+--          FROM (
+--                   SELECT dp.doctor_id, COUNT(dp.patient_id) as ct, dp.dp
+--                   FROM (
+--                            SELECT a.patient_id, a.doctor_id, date_part('year', a.date_and_time) as dp
+--                            FROM appointment as a,
+--                                 doctor as d
+--                            WHERE date_part('year', a.date_and_time) >
+--                                  date_part('year', current_date) - period
+--                              AND a.doctor_id = d.id
+--                        ) as dp
+--                   GROUP BY dp.doctor_id, dp.dp
+--                   HAVING COUNT(dp.doctor_id) >= patients_per_year
+--               ) as noapy
+--          GROUP BY noapy.doctor_id
+--          HAVING COUNT(noapy.ct) >= period
+--             AND SUM(noapy.ct) > total_patients
+--      ) as result,
+--      doctor
+-- WHERE result.doctor_id = doctor.id
+--
+-- $$ language sql;
+
+create or replace function fifth_query() returns setof doctor as
 $$
 SELECT doctor.* /*, patients*/
 FROM (
@@ -149,15 +187,15 @@ FROM (
                            FROM appointment as a,
                                 doctor as d
                            WHERE date_part('year', a.date_and_time) >
-                                 date_part('year', current_date) - period
+                                 date_part('year', current_date) - 10
                              AND a.doctor_id = d.id
                        ) as dp
                   GROUP BY dp.doctor_id, dp.dp
-                  HAVING COUNT(dp.doctor_id) >= patients_per_year
+                  HAVING COUNT(dp.doctor_id) >= 10
               ) as noapy
          GROUP BY noapy.doctor_id
-         HAVING COUNT(noapy.ct) >= period
-            AND SUM(noapy.ct) > total_patients
+         HAVING COUNT(noapy.ct) >= 10
+            AND SUM(noapy.ct) > 100
      ) as result,
      doctor
 WHERE result.doctor_id = doctor.id
